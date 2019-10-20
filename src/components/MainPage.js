cfeb42891dd7973242fae033247da03357c02def
Garfdev <Garfdev.13@gmail.com>
2019-10-20 09:46:58 +0700

    Updated rating updater

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -6,31 +6,32 @@ import NaviBar from "./NaviBar"
 export default function MainPage(props) {
     //FILTER OPTIONALS
     const [name, setName] = useState('')
-    const [year, setYear] = useState(0)
+    const [year, setYear] = useState(2100)
     const [rating, setRating] = useState(0)
     //FILTER FUNCTION
     function moviesFilter(moviesList, name, year, rating){
-        console.log(moviesList)
         if(!!name){
-            moviesList = moviesList.filter(movie => movie.original_title.includes(name) ? true : false )
-        }if(!!rating){
-            moviesList = moviesList.filter(movie => movie.vote_average>rating ? true: false)
+          moviesList = moviesList.filter(movie => movie.original_title.includes(name) ? true : false )
+        }else if(!!rating){
+          console.log(rating)
+          moviesList = moviesList.filter(movie => Number(movie.vote_average)>=rating ? true: false)
         }
+    console.log(moviesList)
     return moviesList
     }
 
     return (
-        <div>          
+        <div>
             <ControlledCarousel />
             <NaviBar
             setName = {setName}
-            setYear = {setYear}
             setRating = {setRating}
+            rating = {rating}
             />
-            <MoviesShowup 
-            movieList={moviesFilter(props.movieList, name, year, rating)} 
+            <MoviesShowup
+            movieList={moviesFilter(props.movieList, name, year, rating)}
             getFirstPage = {props.getFirstPage}
             />
-        </div>  
+        </div>
     )
 }
--- src/components/NaviBar.js
+++ src/components/NaviBar.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import InputRange from 'react-input-range';
+import Slider from 'react-input-slider';
+
+
 
 export default function NaviBar(props) {
+
+  const [state, setState] = useState({ x: 10});
+
+
+
+
     return (
         <div className="container navi-container">
-            <input placeholder="Type smth ehre" onChange={event => {
+            <input id="movieSearch" placeholder="Search your movies here..." onChange={event => {
                 console.log(event.target.value)
                 props.setName(event.target.value)
             }} />
-            <input type="range" min={0} max={10} step={0.1} onChange={e => props.setRating(e.target.value)} // don't set state on all change as react will re-render
-            />
+            <div className="slider">
+              <Slider
+                axis="x"
+                x = {props.rating}
+                xmax = {10}
+                xmin = {0}
+                xstep = {0.5}
+                onChange = {event => props.setRating(event.x)}
+                valuelabeldisplay="auto"
+              />
+            </div>
+
+
         </div>
     )
 }
-
