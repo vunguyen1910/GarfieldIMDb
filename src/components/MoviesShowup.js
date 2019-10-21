0c6c1e53f79a0a9b4f522371bf95b2510d6dbdaa
Viktor_nguyen <melodycenter19@gmail.com>
2019-10-21 15:03:03 +0700

    popup trailer

--- src/components/MoviesShowup.js
+++ src/components/MoviesShowup.js
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faStarHalf } from "@fortawesome/free-solid-svg-icons";
-import ShowMore from "./ShowMore"
+import ReactModal from "react-modal";
+import YouTube from "@u-wave/react-youtube";
 
 const Genres = {
   28: "Action",
@@ -42,24 +43,34 @@ const Genres = {
   37: "Western"
 };
 
-
-
 export default function MoviesShowup(props) {
   const [collapse, setCollapse] = useState(false);
-
+  const [isOpen, setIsOpen] = useState(false);
+  const [idTrailer, setIdTrailer] = useState(null);
   let cardStyle = {
-      color: `${collapse ? "black" : "white"}`
-  }
-
+    color: `${collapse ? "black" : "white"}`
+  };
+  const showModal = (id) => {
+    getTrailer(id);
+    setIsOpen(true)
+  };
+  const getTrailer = async (id) => {
+    const response = await fetch(
+      `http://api.themoviedb.org/3/movie/${id}/videos?api_key=a3bab0e1a8803be8f21fc84e04666349`
+    );
+    const data = await response.json();
+    setIdTrailer(data.results[0].key)
+  };
   return (
     <div className="container movies-container">
       {props.movieList.map(movie => {
         return (
-          <Card  key={movie.id}>
+          <Card key={movie.id}>
             <Card.Img
               className="cards"
               variant="top"
-              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path && movie.poster_path}`}
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path &&
+                movie.poster_path}`}
               alt="This API is suck"
             />
             <Card.ImgOverlay className="noselect">
@@ -76,15 +87,30 @@ export default function MoviesShowup(props) {
             </Card.ImgOverlay>
             <Card.Body>
               <div className="movie-tag">
-                {movie.genre_ids.map((gen,idx) => {
-                  return <button  key={idx} className="tag-button">{Genres[gen]}</button>;
+                {movie.genre_ids.map((gen, idx) => {
+                  return (
+                    <button key={idx} className="tag-button">
+                      {Genres[gen]}
+                    </button>
+                  );
                 })}
               </div>
-              <Card.Link href="#">Watch Trailer</Card.Link>
+              <button onClick={() => {
+                showModal(movie.id)
+                }
+              }className="btn btn-primary btn-lg"> ViewTrailer</button>
             </Card.Body>
           </Card>
         );
       })}
+      <ReactModal
+        closeTimeoutMS={2000}
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+        onClick={() => setIsOpen(false)}
+      >
+        {isOpen === true ? <YouTube video={`${idTrailer}`} autoplay width="100%" height="100%" /> : ""}
+      </ReactModal>
     </div>
   );
 }
